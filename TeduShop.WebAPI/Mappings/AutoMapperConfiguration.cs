1dcfe38e3a2c32cf0c7d4dd1efc307f405a097ab
dieptran43 <levandiep44@gmail.com>
2017-06-02 11:01:11 +0700

    First Committed

---
+++ TeduShop.WebAPI/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostViewModel>();
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+                cfg.CreateMap<Tag, TagViewModel>();
+                cfg.CreateMap<ProductCategory, ProductCategoryViewModel>();
+                cfg.CreateMap<Product, ProductViewModel>();
+                cfg.CreateMap<ProductTag, ProductTagViewModel>();
+                cfg.CreateMap<Footer, FooterViewModel>();
+                cfg.CreateMap<Slide, SlideViewModel>();
+                cfg.CreateMap<Page, PageViewModel>();
+                cfg.CreateMap<ContactDetail, ContactDetailViewModel>();
+                cfg.CreateMap<AppRole, ApplicationRoleViewModel>();
+                cfg.CreateMap<AppUser, AppUserViewModel>();
+                cfg.CreateMap<Function, FunctionViewModel>();
+                cfg.CreateMap<Permission, PermissionViewModel>();
+                cfg.CreateMap<ProductImage, ProductImageViewModel>();
+                cfg.CreateMap<ProductQuantity, ProductQuantityViewModel>();
+                cfg.CreateMap<Color, ColorViewModel>();
+                cfg.CreateMap<Size, SizeViewModel>();
+                cfg.CreateMap<Order, OrderViewModel>();
+                cfg.CreateMap<OrderDetail, OrderDetailViewModel>();
+            });
+        }
+    }
+}
